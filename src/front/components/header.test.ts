43eef17183a75f26d9ab76144f03663d71b04b51
RocioLdlT <r.delatorre.f95@gmail.com>
2026-05-26 11:09:54 +0100

    Add structure to header component

---
+++ src/front/components/header.test.ts
@@ -0,0 +1,11 @@
+describe('Given createHeader function', () => {
+    afterEach(() => {
+        document.body.innerHTML = '';
+    });
+    describe('When createHeader is called', () => {
+        test('Then the title "Productos" is in the document', () => {});
+
+        test('Then the logo is in the document', () => {});
+    });
+    test('Then the "Add" button is in the document', () => {});
+})
