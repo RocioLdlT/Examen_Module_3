4f7a68e2fdbd0931fa589db0d5419eef16a7b087
RocioLdlT <r.delatorre.f95@gmail.com>
2026-05-26 11:21:59 +0100

    Add test for header title

--- src/front/components/header.test.ts
+++ src/front/components/header.test.ts
@@ -1,18 +1,25 @@
-import { createHeader } from "./header";
+import { createHeader } from './header';
+import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom'
+
 
 describe('Given createHeader function', () => {
     afterEach(() => {
         document.body.innerHTML = '';
     });
     describe('When createHeader is called', () => {
+        //Arrange
         beforeEach(() => {
             createHeader();
         });
         test('Then the title "Productos" is in the document', () => {
-            
+            //Act
+            const title = screen.getByRole('heading');
+            //Assert
+            expect(title).toBeInTheDocument();
         });
 
         test('Then the logo is in the document', () => {});
     });
     test('Then the "Add" button is in the document', () => {});
-})
+});
