e37e9fec10c0c1a916da50bf0cff400f35f79602
RocioLdlT <r.delatorre.f95@gmail.com>
2026-05-26 12:47:52 +0100

    Add act y assert to two test in form.add

--- src/front/components/form.add.test.ts
+++ src/front/components/form.add.test.ts
@@ -1,4 +1,7 @@
 import { createFormAdd } from "./form.add";
+import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Given createFormAdd function', () => {
 
@@ -14,14 +17,16 @@ describe('Given createFormAdd function', () => {
 
         test('Then the form is in the document', () => {
             //Act
+            const form = screen.getByRole('form')
             //Assert
+            expect(form).toBeInTheDocument()
            
         });
 
-        test('Then the form can be filled and submitted', () => {
-            //Act
-            //Assert
-
+        test('Then the form can be filled and submitted', async() => {
+            //Act y Assert
+           await userEvent.type(screen.getByRole('textbox', { name: /name/i }), 'Product 1');
+           await userEvent.click(screen.getByRole('button', { name: /crear/i }));
         });
     });
 });
