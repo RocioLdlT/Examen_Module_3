e1647fe2e0e6c1e20163324196de0eb59c34acdb
RocioLdlT <r.delatorre.f95@gmail.com>
2026-05-26 12:49:04 +0100

    Fix header.test and add .toHaveAttribute

--- src/front/components/header.test.ts
+++ src/front/components/header.test.ts
@@ -3,37 +3,46 @@ import { screen } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 
+ 
 describe('Given createHeader function', () => {
+
     afterEach(() => {
         document.body.innerHTML = '';
     });
+ 
     describe('When createHeader is called', () => {
+ 
         //Arrange
         beforeEach(() => {
             createHeader();
         });
+ 
         test('Then the title "Productos" is in the document', () => {
             //Act
             const title = screen.getByRole('heading');
             //Assert
             expect(title).toBeInTheDocument();
-            expect(title).toHaveTextContent('Productos');
+            expect(title).toHaveTextContent('Productos')
         });
-
+ 
         test('Then the logo is in the document', () => {
             //Act
             const logo = screen.getByAltText(/logo de la empresa/i);
             //Assert
             expect(logo).toBeInTheDocument();
-            // expect(logo).toHaveAttribute(src="favicon.png");
+            expect(logo).toHaveAttribute('src', 'favicon.png')
         });
-    });
+ 
     test('Then the "Add" button is in the document', async () => {
         //Act
-        const btnElement = screen.getByRole('button');
-        await userEvent.click(btnElement);
-        //Assert
-        expect(btnElement).toBeInTheDocument();
-        expect(btnElement).toHaveAttribute('aria-expanded ="false"');
+    const btnElement = screen.getByRole('button');
+    await userEvent.click(btnElement);
+     //Assert
+    expect(btnElement).toBeInTheDocument();
+    expect(btnElement).toHaveAttribute('aria-controls', "add")
+    expect(btnElement).toHaveAttribute('aria-expanded', "false")
+
+    
+});
     });
 });
