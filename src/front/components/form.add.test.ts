f91782557a27b2e790f0310952995bab5a9cbe8a
RocioLdlT <r.delatorre.f95@gmail.com>
2026-05-26 12:17:37 +0100

    Add the structure test to form.add.ts

---
+++ src/front/components/form.add.test.ts
@@ -0,0 +1,23 @@
+import { createFormAdd } from "./form.add";
+
+describe('Given createFormAdd function', () => {
+
+    afterEach(() => {
+        document.body.innerHTML = '';
+    });
+
+    describe('', () => {
+        //Arrange
+        beforeEach(() => {
+            createFormAdd();
+        });
+
+        test('', () => {
+           
+        });
+
+        test('', () => {
+
+        });
+    });
+});
